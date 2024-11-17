5c03e8b8804089c56f7e94dac87d0f35031c2667
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-17 19:44:26 +0530

    Update
    Inspired by Stripe.com
    ready

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 "use strict";
-const triggers = document.querySelectorAll(".navbar > li");
+const triggers = document.querySelectorAll('.lists>li');
 const background = document.querySelector(".dropdown-bg");
-const nav=document.querySelector(".navbar")
+const nav = document.querySelector(".navbar");
+function HandleEnter() {
 
+    background.classList.add("opens");
+    this.classList.add("trigger-enter");
 
-function HandleEnter(e) {
-    console.log("Enter");
+    setTimeout(() => {
+
+        if (this.classList.contains("trigger-enter")) {
+            this.classList.add("trigger-enter-active");
+        }
+    }, 100);
+
+    const dropdowns = this.querySelector(".item-info");
+    if (!dropdowns) return;
+    const dropCoord = dropdowns.getBoundingClientRect();
+    const navCoord = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropCoord.height,
+        width: dropCoord.width,
+        top: dropCoord.top-navCoord.top,
+        left: dropCoord.left-navCoord.left,
+    };
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px,${coords.top}px)`);
+    
 }
 
-function HandleLeave(e) {
-    console.log("Leave");
+function HandleLeave() {
+    background.classList.remove("opens");
+    this.classList.remove("trigger-enter", "trigger-enter-active");
+    
 }
-console.log(triggers);
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', HandleEnter));
 triggers.forEach(trigger => trigger.addEventListener('mouseleave', HandleLeave));
